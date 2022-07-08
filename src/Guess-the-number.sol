e0946e4df55f75e117a453e6708c7139f35528d8
Marissccal <84743174+Marissccal@users.noreply.github.com>
2022-07-07 21:39:18 -0300

    Initial Commit

---
+++ src/Guess-the-number.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.13 ;
+
+contract GuessTheNumberChallenge {
+    uint8 answer = 42;
+
+    constructor () payable {
+        require(msg.value == 1 ether);
+    }
+
+    function isComplete() public view returns (bool) {
+        return address(this).balance == 0;
+    }
+
+    function guess(uint8 n) public payable {
+        require(msg.value == 1 ether, "Pone mas guita");
+
+        if (n == answer) {
+            payable(msg.sender).transfer(2 ether);
+        }
+    }
+}
---
+++ test/guess-the-number.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "../src/Guess-the-number.sol";
+
+
+contract ContractTest is Test {
+    GuessTheNumberChallenge target;
+    address player = vm.addr(1);
+
+    function setUp() public {
+        target = new GuessTheNumberChallenge{value: 1 ether}();
+        vm.label(address(target), "Challenge");
+        vm.label(player, "Player");
+        vm.deal(player, 1 ether);
+    }
+
+    function testChallenge() public {
+        vm.startPrank(address(player));
+        target.guess{value: 1 ether}(42);
+        assertTrue(target.isComplete());
+    }
+}
