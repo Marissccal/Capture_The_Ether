7c1b31b6ce011459fac60b48e3beba172966d0a9
Marissccal <84743174+Marissccal@users.noreply.github.com>
2022-07-31 23:30:22 -0300

    random number

---
+++ test/guess-the-random.number.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "../src/Guess-the-random-number.sol";
+
+
+contract Challenge3Test is Test {
+    GuessTheRandomNumberChallenge target;
+    address player = vm.addr(1);
+
+    function setUp() public {
+        target = new GuessTheRandomNumberChallenge{value: 1 ether}();
+        vm.label(address(target), "Challenge");
+        vm.label(player, "Player");
+        vm.deal(player, 1 ether);
+    }
+
+    function testChallenge() public {
+        uint8 answer = uint8(uint256(vm.load(address(target), 0)));
+        vm.startPrank(address(player));
+        target.guess{value: 1 ether}(answer);
+        assertTrue(target.isComplete());
+    }
+}
